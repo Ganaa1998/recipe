6a7b65af927a2026666e5c7bee12f84c49df1f8d
Gankhulug <gankhulug1016@gmail.com>
2020-10-29 17:19:37 +0800

    docs, search, gitpage ashiglahiin tuld

--- src/js/model/search.js
+++ src/js/model/search.js
@@ -5,8 +5,6 @@ export default class Search {
     constructor(query) {
         this.query = query
     }
-
-
     async doSearch() {
         try {
             let result = await axios(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`);
